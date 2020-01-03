ffb3ffd549875b51e916a406f373f38e1dda641b
flongford <flongford@enthought.com>
2020-01-03 13:52:28 +0000

    FEAT: included ci directory and setup.py file for installation of module as a force-bdss extension

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0.dev0"
+
+
+# Read description
+with open('README.md', 'r') as readme:
+    README_TEXT = readme.read()
+
+
+def write_version_py():
+    filename = os.path.join(
+        os.path.dirname(__file__),
+        'force_nevergrad',
+        'version.py')
+    ver = "__version__ = '{}'\n"
+    with open(filename, 'w') as fh:
+        fh.write(ver.format(VERSION))
+
+
+write_version_py()
+
+setup(
+    name="force_nevergrad",
+    version=VERSION,
+    entry_points={
+            "force.bdss.extensions": [
+                "force_nevergrad = "
+            ]
+        },
+    packages=find_packages(),
+    install_requires=[
+            "force_bdss >= 0.3.0",
+        ]
+)
