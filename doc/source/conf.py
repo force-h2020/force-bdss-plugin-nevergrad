66ed3ae057bde63bd824483ee0b1a13e0135167c
sparsonslab <sparsonslab@gmail.com>
2020-05-04 17:43:17 +0100

    copied doc/source/conf from other plugin

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-#
-# FORCE documentation build configuration file, created by
-# sphinx-quickstart on Mon Feb 02 16:26:16 2015.
 
 import sphinx.environment
 from docutils.utils import get_source_line
 import sys
 import os
 
-def _warn_node(self, msg, node, **kwargs):
-    if not msg.startswith('nonlocal image URI found:'):
-        self._warnfunc(msg, '%s:%s' % get_source_line(node), **kwargs)
 
 sys.path.insert(0, os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "..", "..")
@@ -18,27 +12,23 @@ sys.path.insert(0, os.path.join(
 
 from force_nevergrad.version import __version__ as RELEASE
 
+MOCK_MODULES = []
+
+
+def _warn_node(self, msg, node, **kwargs):
+    if not msg.startswith('nonlocal image URI found:'):
+        self._warnfunc(msg, '%s:%s' % get_source_line(node), **kwargs)
+
+
 sphinx.environment.BuildEnvironment.warn_node = _warn_node
+
 
 def mock_modules():
     import sys
 
     from unittest.mock import MagicMock
 
-    try:
-        import tables
-    except ImportError:
-        MOCK_MODULES = ['tables']
-    else:
-        MOCK_MODULES = []
-
-    try:
-        import numpy
-    except ImportError:
-        MOCK_MODULES.append('numpy')
-
     class Mock(MagicMock):
-
         @classmethod
         def __getattr__(cls, name):
             return Mock()
@@ -49,13 +39,11 @@ def mock_modules():
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
     print('mocking {}'.format(MOCK_MODULES))
 
-# mock_modules()
-
-# General Configuration
-# ---------------------
+mock_modules()
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
@@ -63,33 +51,18 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'traits.util.trait_documenter'
 ]
-
-try:
-    import enthought_sphinx_theme
-
-    html_theme_path = [enthought_sphinx_theme.theme_path]
-    html_theme = 'enthought'
-except ImportError as exc:
-    import warnings
-    msg = '''Can't find Enthought Sphinx Theme, using default.
-                Exception was: {}
-                Enthought Sphinx Theme can be downloaded from
-                https://github.com/enthought/enthought-sphinx-theme'''
-    warnings.warn(RuntimeWarning(msg.format(exc)))
-
 
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'FORCE Nevergrad'
-copyright = u'2019, FORCE, EU H2020 Project'
+project = u'FORCE'
+copyright = u'2017, FORCE Project'
 version = ".".join(RELEASE.split(".")[0:3])
 release = RELEASE
-exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'classic'
 html_static_path = ['_static']
-htmlhelp_basename = 'Forcedoc'
+html_logo = '_static/force_logo.png'
+htmlhelp_basename = 'FORCEdoc'
 intersphinx_mapping = {'http://docs.python.org/': None}
